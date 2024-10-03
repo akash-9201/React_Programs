39129cd486b2126891849a37e0e7ea9cb70798d7
akash-9201 <akashsaravalkar9201@gmail.com>
2024-10-03 23:26:24 +0530

    React crud opration

---
+++ CRUD/src/EmployeeData.js
@@ -0,0 +1,24 @@
+export const EmployeeData=[{
+    id:1,
+    firstName:"Rohit",
+    lastName: "Patil",
+    age:22
+},
+{
+    id:2,
+    firstName:"Vaibhav",
+    lastName: "Patil",
+    age:24
+},
+{
+    id:3,
+    firstName:"Rohan",
+    lastName: "Savekar",
+    age:25
+},{
+    id:4,
+    firstName:"kishor",
+    lastName: "Patil",
+    age:20
+}
+]
